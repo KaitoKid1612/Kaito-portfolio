b9956f9463d44198b7c3a5f2eca08b931b7fd77f
Kaito <vielv@waocon-sg.vn>
2024-03-01 18:58:45 +0700

    feat: display projects

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed $id
+ * @property mixed $name
+ * @property mixed $description
+ * @property mixed $image
+ * @property mixed $skill_id
+ * @property mixed $project_url
+ */
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'image' => asset('storage/' . $this->image),
+            'skill_id' => $this->skill_id,
+            'project_url' => $this->project_url,
+        ];
+    }
+}
