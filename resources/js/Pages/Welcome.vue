470219c2e0474a35e9b21d9f0636b6dbc161434d
Kaito <vielv@waocon-sg.vn>
2024-03-14 08:59:37 +0700

    fix: merge main

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,13 +1,21 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import Frontend from "@/Layouts/Frontend.vue";
+import Hero from "@/Components/Frontend/Hero.vue";
 
 </script>
 
 <template>
     <Head title="Welcome to my Portfolio" />
     <Frontend>
-
+        <!-- Hero primary -->
+        <Hero />
+        <!-- Promote tail-100 -->
+        <!-- About secondary -->
+        <!-- Skills tail-100 -->
+        <!-- Portfolio primary -->
+        <!-- Services secondary -->
+        <!-- Contact primary -->
     </Frontend>
 </template>
 
