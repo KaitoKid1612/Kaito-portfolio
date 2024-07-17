f13d4e5a65f787c2eef5388cc01cb221f1234615
Lê Viết Việt <leviet1612@gmail.com>
2024-07-17 12:55:50 +0700

    fix: fix padding header item

--- resources/js/Components/Frontend/Header.vue
+++ resources/js/Components/Frontend/Header.vue
@@ -1,8 +1,6 @@
 <script setup>
-
-import  { ref } from "vue";
+import { ref } from "vue";
 const showMobileMenu = ref(false);
-
 </script>
 
 <template>
@@ -12,28 +10,39 @@ const showMobileMenu = ref(false);
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a>
-            <button @click="showMobileMenu = !showMobileMenu" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <button @click="showMobileMenu = !showMobileMenu" data-collapse-toggle="navbar-default" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-            <div class="w-full md:block md:w-auto" id="navbar-default" :class="{hidden : showMobileMenu}">
-                <ul class="flex flex-col p-4 mt-4 rounded-lg border border-light-tail-500 dark:border-dark-navy-100 md:flex-row md:space-x-8 md:mt-0 md:font-medium md:border-0">
+            <div class="w-full md:block md:w-auto" id="navbar-default" :class="{ hidden: showMobileMenu }">
+                <ul
+                    class="flex flex-col justify-center items-center p-4 mt-4 rounded-lg border border-light-tail-500 dark:border-dark-navy-100 md:flex-row md:space-x-8 md:mt-0 md:font-medium md:border-0">
                     <li>
-                        <a href="#" class="block py-2 pl-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100" aria-current="page">Home</a>
+                        <a href="#"
+                            class="block py-2 p-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100"
+                            aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pl-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100">About</a>
+                        <a href="#"
+                            class="block py-2 p-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100">About</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pl-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100">Services</a>
+                        <a href="#"
+                            class="block py-2 p-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100">Services</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pl-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100">Pricing</a>
+                        <a href="#"
+                            class="block py-2 p-3 dark:hover:text-white bg-light-tail-500 rounded dark:text-dark-navy-100 hover:text-light-tail-100">Pricing</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        <a href="#"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -41,6 +50,4 @@ const showMobileMenu = ref(false);
     </nav>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
