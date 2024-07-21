fbc4db1a578fe966a625b1662e85617ed09c0bd6
Lê Viết Việt <leviet1612@gmail.com>
2024-07-21 23:50:07 +0700

    fix: fix max-width services container

--- resources/js/Components/Frontend/Projects.vue
+++ resources/js/Components/Frontend/Projects.vue
@@ -82,3 +82,16 @@ const filterProjects = (id) => {
     </section>
   </div>
 </template>
+
+<style scoped>
+@media (min-width: 1536px) {
+    .container {
+        max-width: 1280px;
+    }
+}
+
+.container {
+    padding-right: 0px;
+    padding-left: 0px;
+}
+</style>
